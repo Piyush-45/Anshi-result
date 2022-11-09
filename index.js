24ee90f91ca429fe62a9210692b15f38a77de234
Piyush Tyagi <tyagipiyush2233@gmail.com>
2022-11-09 15:25:56 +0530

    hi

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var attempt = 3; // Variable to count number of attempts.
 function validate(){
 var username = document.getElementById("username").value;
 // var password = document.getElementById("password").value;
-if ( username == "210018001734" ){
+if ( username == "210901110111" ){
 
 window.location = "https://drive.google.com/file/d/1-OfnIB1gC5C2SFxJg5BgdevAw60NwtzJ/view"; // Redirecting to other page.
 return false;
